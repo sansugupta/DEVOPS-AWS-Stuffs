9cdd5271501aad061cf3450764e5149807a1bd89
Sanskar <Sanskargupta966@gmail.com>
2023-10-26 16:19:15 +0530

    added

---
+++ aws_resource_tracker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#######################################
+# Author: Sanskar
+# Date: 11th-Jan
+# Version: v1
+#
+# This Script will report the AWS resource usage
+#######################################
+#To Put Script in Debug Mode
+set -x
+# AWS S3
+# AWS Ec2
+# AWS Lambda
+# AWS IAM Users
+# list s3 buckets
+echo"Print list of s3 buckets"
+aws s3 ls
+#list EC2 Instances
+echo "Print list of ec2 buckets"
+aws ec2 describe-instances
+#list lambda
+echo "Print list of lambda functions"
+aws lambda list-funtions
+#list IAM users
+echo "Print list of IAM Users"
+aws iam list-users
